92f9bb56f03f4f4d6c9f02fd62feb881f16075a3
Marcos Zuniga <mzuniga@ergio.com.ar>
2012-07-04 11:20:57 -0300

    Minor fix to Receipt to Invoices Window.
    Change default logic of Ad_Client field.

---
+++ migration/oracle/0040_Minor_Change_to_Receipt_Invoices_Window.sql
@@ -0,0 +1,10 @@
+-- 04/07/2012 10:59:56 ART
+-- Minor_Change_to_Receipt_Invoices_Window.sql
+UPDATE AD_Field SET DefaultValue=NULL,Updated=TO_DATE('2012-07-04 10:59:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1000197
+;
+
+-- 04/07/2012 11:01:15 ART
+-- Minor_Change_to_Receipt_Invoices_Window.sql
+UPDATE AD_Field SET DefaultValue='@#AD_Org_ID@',Updated=TO_DATE('2012-07-04 11:01:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1000198
+;
+
