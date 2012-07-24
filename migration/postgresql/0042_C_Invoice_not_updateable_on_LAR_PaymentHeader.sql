022dde55a192b7f3a70111485beb47a4ab4d5421
Marcos Zuniga <mzuniga@ergio.com.ar>
2012-07-23 23:33:50 -0300

    Set not updateable C_Invoice_ID column on LAR_PaymentHeader table.

---
+++ migration/postgresql/0042_C_Invoice_not_updateable_on_LAR_PaymentHeader.sql
@@ -0,0 +1,5 @@
+-- 23/07/2012 23:04:11 ART
+-- C_invoice_id not updateable on Payment and Receipt Headers
+UPDATE AD_Column SET IsUpdateable='N',Updated=TO_TIMESTAMP('2012-07-23 23:04:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000253
+;
+
