90672e41d375070bb421287e4a61c133d04e4525
Emiliano Pereyra <epereyra@ergio.com.ar>
2012-08-08 12:57:31 -0300

    Fix read only behavior for fiscal fields.

--- migration/postgresql/0044_Fix_And_Improve_CreditNotes_Behavior.sql
+++ migration/postgresql/0044_Fix_And_Improve_CreditNotes_Behavior.sql
@@ -3,12 +3,12 @@
 UPDATE AD_Column SET Callout='org.compiere.model.CalloutInvoice.docType;ar.com.ergio.model.LAR_Callouts.documentLetter',Updated=TO_TIMESTAMP('2012-08-07 21:39:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3781
 ;
 
--- 07/08/2012 23:10:49 ART
+-- 08/08/2012 11:44:49 ART
 -- Avoid updates values set by fiscal printer
-UPDATE AD_Column SET IsUpdateable='N',Updated=TO_TIMESTAMP('2012-08-07 23:10:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000039
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2012-08-08 11:44:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3000025
 ;
 
--- 07/08/2012 23:11:24 ART
+-- 08/08/2012 11:45:02 ART
 -- Avoid updates values set by fiscal printer
-UPDATE AD_Column SET IsUpdateable='N',Updated=TO_TIMESTAMP('2012-08-07 23:11:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000038
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2012-08-08 11:45:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3000026
 ;
