1c36da99f1cf0f27f4ca2f9d856a3d4bc1a8cfd6
Marcos Zuniga <mzuniga@ergio.com.ar>
2012-09-20 12:32:09 -0300

    Minor fix to script 0047
    Set IsAllowCopy='N' on WithholdingAmt Column.

--- migration/oracle/0047_NOT_Allow_Copy_For_Columns_in_C_Invoice.sql
+++ migration/oracle/0047_NOT_Allow_Copy_For_Columns_in_C_Invoice.sql
@@ -1,40 +1,46 @@
 -- 03/09/2012 10:13:55 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:13:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000038
 ;
 
 -- 03/09/2012 10:14:31 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:14:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=5025
 ;
 
 -- 03/09/2012 10:15:12 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:15:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000039
 ;
 
 -- 03/09/2012 10:15:43 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:15:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4304
 ;
 
 -- 03/09/2012 10:18:24 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:18:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000040
 ;
 
 -- 03/09/2012 10:18:31 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:18:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000041
 ;
 
 -- 03/09/2012 10:19:40 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:19:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4649
 ;
 
 -- 03/09/2012 10:21:21 ART
--- No permiter copiar estos campos al utilizar la opción duplicar - Not allow copy
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-03 10:21:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3497
 ;
+
+-- 20/09/2012 12:26:36 ART
+-- No permite copiar estos campos al utilizar la opción duplicar - Not allow copy
+UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2012-09-20 12:26:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000163
+;
+
 
