6bc498e5baf6151b1a5d427d0e052a2471eec112
Emiliano Pereyra <epereyra@ergio.com.ar>
2014-07-03 14:51:06 -0300

    Issue #65: Correción de filtro en referencia LAR_POS AD_Org

---
+++ migration/postgresql/0058_ISSUE-65.sql
@@ -0,0 +1,8 @@
+-- 03/07/2014 12:24:31 ART
+-- LAR #65: Completamiento e Impresion Fiscal de Factura desde Remito
+UPDATE AD_Ref_Table SET WhereClause='C_POS.IsActive=''Y'' AND C_POS.IsShipment=''N''',Updated=TO_TIMESTAMP('2014-07-03 12:24:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=3000005
+;
+
+-- Registración de script
+SELECT register_migration_script_lar('0058_ISSUE-65.sql', 'LAR', '')
+;
