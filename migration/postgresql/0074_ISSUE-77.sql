db2ee49ff541ba3edf7bc88707bc2ff9a4e5c193
Franco Chiappano <fchiappano@comit.com.ar>
2016-07-22 21:37:15 -0300

    ISSUE #74: Restricción no copiar campos CAE.
    Se cambio la config. de manera que al copiar una factura, no seas
    copiados todos aquellos campos relacionados al CAE de la misma.

---
+++ migration/postgresql/0074_ISSUE-77.sql
@@ -0,0 +1,18 @@
+-- 22/07/2016 21:13:43 ART
+-- ISSUE #77: Facturación Electronica.
+UPDATE AD_Field SET IsAllowCopy='N',Updated=TO_TIMESTAMP('2016-07-22 21:13:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3002136
+;
+
+-- 22/07/2016 21:13:49 ART
+-- ISSUE #77: Facturación Electronica.
+UPDATE AD_Field SET IsAllowCopy='N',Updated=TO_TIMESTAMP('2016-07-22 21:13:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3002142
+;
+
+-- 22/07/2016 21:13:56 ART
+-- ISSUE #77: Facturación Electronica.
+UPDATE AD_Field SET IsAllowCopy='N',Updated=TO_TIMESTAMP('2016-07-22 21:13:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3002138
+;
+
+-- Registración de script
+SELECT register_migration_script_lar('0074_ISSUE-77', 'LAR', '')
+;
