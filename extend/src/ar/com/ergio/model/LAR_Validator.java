5c02390c244134acbbf1e2862d1c819f11bca3da
Marcos Zúñiga <mzuniga@comit.com.ar>
2018-01-02 00:02:15 -0300

    ISSUE #55: Determinación Tipo de Documento
    Se evita reemplazar el tipo de documento cuando se está facturando desde
    la ventana de Facturas, allí se selecciona manualmente el tipo de
    documento a generar.

--- extend/src/ar/com/ergio/model/LAR_Validator.java
+++ extend/src/ar/com/ergio/model/LAR_Validator.java
@@ -326,9 +326,15 @@ import ar.com.ergio.util.LAR_Utils;
 
             // @emmie @mzuniga Se abstrae la forma de recuperar el tipo de documento
             // @mzuniga Se considera el tipo de documento base (NC o Factura)
+            final MDocType docType;
             MDocType dt_orig = new MDocType(invoice.getCtx(), invoice.getC_DocTypeTarget_ID(), invoice.get_TrxName());
+            if (invoice.getC_Order_ID() != 0)
+            {
             final FindInvoiceDocType findDocType = new FindInvoiceDocType(bp, c_POS_ID, ad_Org_ID, dt_orig.getDocBaseType());
-            final MDocType docType = findDocType.getDocType();
+            docType = findDocType.getDocType();
+            }
+            else
+                docType = dt_orig;
 
             // Check retrieved doctype
             if (docType == null) {
@@ -352,7 +358,7 @@ import ar.com.ergio.util.LAR_Utils;
                             .getDocSubTypeSO().equals(MDocType.DOCSUBTYPESO_StandardOrder))))
                 invoice.setC_DocTypeTarget_ID(docType.getC_DocType_ID());
 
-            invoice.set_ValueOfColumn("LAR_DocumentLetter_ID", findDocType.getLAR_DocumentLetter_ID());
+            invoice.set_ValueOfColumn("LAR_DocumentLetter_ID", docType.get_ValueAsInt("LAR_DocumentLetter_ID"));
             if (!invoice.save()) {
                 return "No se pudo cambiar el tipo de documento";
             }
