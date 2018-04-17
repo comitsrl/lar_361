2c4b8084c26fcf0951af18704cba04fb71e83648
Franco Chiappano <fchiappano@comit.com.ar>
2018-04-17 19:18:38 -0300

    ISSUE #103: Se corrige script de migración

--- migration/oracle/0107_ISSUE-103.sql
+++ migration/oracle/0107_ISSUE-103.sql
@@ -1,5 +1,5 @@
 -- Corrección tipo de columna
-﻿ALTER TABLE M_RmaLine ALTER COLUMN PriceList TYPE numeric;
+ALTER TABLE M_RmaLine ALTER COLUMN PriceList TYPE numeric;
 
 -- Registración de script
 SELECT register_migration_script_lar('0107_ISSUE-103.sql', 'LAR', '') FROM dual
