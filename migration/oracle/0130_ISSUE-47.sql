b482061eee0b044f0a5c3db040f4051f2d390697
Franco Chiappano <fchiappano@comit.com.ar>
2019-06-05 19:48:43 -0300

    ISSUE #47: Corrección menor en script de migración

--- migration/oracle/0130_ISSUE-47.sql
+++ migration/oracle/0130_ISSUE-47.sql
@@ -1006,6 +1006,11 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Help,Description,Name, IsTran
 UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=3006523
 ;
 
+-- 05/06/2019 19:41:41 ART
+-- ISSUE #47: Generación Automática de Listas de Precios.
+UPDATE AD_Column SET AD_Process_ID=3000359,Updated=TO_DATE('2019-06-05 19:41:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3003478
+;
+
 -- Registración de script
 SELECT register_migration_script_lar('0130_ISSUE-47.sql', 'LAR', '') FROM dual
 ;
