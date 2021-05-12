d50ea4065700e276b1cc485b3efc818f55a74da1
Marcos Zúñiga <mzuniga@comit.com.ar>
2021-05-12 18:15:26 -0300

    ISSUE #141: Autoimpresor Remitos
    Se habilita la selección de PDVs de tipo remito desde la configuración
    de tipos de documento.

--- migration/postgresql/0170_ISSUE-141.sql
+++ migration/postgresql/0170_ISSUE-141.sql
@@ -701,6 +701,11 @@ UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=3007037
 UPDATE AD_Field SET SeqNo=520,IsDisplayed='Y' WHERE AD_Field_ID=3007035
 ;
 
+-- 12/05/2021 10:53:30 ART
+-- ISSUE #141: Autoimpresor Remitos
+UPDATE AD_Ref_Table SET WhereClause='C_POS.IsActive=''Y''',Updated=TO_TIMESTAMP('2021-05-12 10:53:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=3000005
+;
+
 -- Registración de script
 SELECT register_migration_script_lar('0170_ISSUE-141.sql', 'LAR', '')
 ;
