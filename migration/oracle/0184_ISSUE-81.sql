cf71bacdcb125d0aa343929af437fc68b6c363d8
Marcos Zúñiga <mzuniga@comit.com.ar>
2021-07-21 13:37:17 -0300

    ISSUE #81: Recibos con Cheque Propio
    Se ajusta para que el tipo de cheque emitido no sea obligatorio en
    recibos, únicamente en órdenes de pago.

---
+++ migration/oracle/0184_ISSUE-81.sql
@@ -0,0 +1,8 @@
+-- 21/07/2021 13:27:27 ART
+-- BUG #377: Ajustes de Ventanas y traducciones
+UPDATE AD_Column SET MandatoryLogic='@IsSOTrx@=N & @TenderType@=K & @EsRetencionSufrida@=N',Updated=TO_DATE('2021-07-21 13:27:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3002109
+;
+
+-- Registración de script
+SELECT register_migration_script_lar('0184_ISSUE-81.sql', 'LAR', '') FROM dual
+;
