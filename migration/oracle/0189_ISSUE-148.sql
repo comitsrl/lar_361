41b7ac0108cd6c57cc5bf932a0eb997acacfa83b
Franco Chiappano <fchiappano@localhost.localdomain>
2021-08-04 13:35:08 -0300

    ISSUE #148: Readecuación de pestaña C.Bancaria SdN.

---
+++ migration/oracle/0189_ISSUE-148.sql
@@ -0,0 +1,403 @@
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4107
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4096
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4097
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4103
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4113
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4102
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4112
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4101
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4098
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4106
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4099
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4108
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=6298
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4111
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4109
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=13699
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4110
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=2200
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 12:43:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:43:20 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 12:43:20 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=100,IsDisplayed='Y' WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 12:43:40 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET IsMandatory=NULL,Updated=TO_DATE('2021-08-04 12:43:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2200
+;
+
+-- 04/08/2021 12:44:16 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=2200
+;
+
+-- 04/08/2021 12:44:16 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 12:44:16 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:44:16 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 12:44:16 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 12:45:19 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_DATE('2021-08-04 12:45:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 12:45:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_DATE('2021-08-04 12:45:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:45:30 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_DATE('2021-08-04 12:45:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 12:46:45 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 12:46:45 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 12:46:45 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:46:45 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 12:47:00 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:47:00 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 12:51:13 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Column (AD_Column_ID,AD_Table_ID,EntityType,Version,IsMandatory,IsTranslated,IsIdentifier,IsParent,FieldLength,IsSelectionColumn,AD_Reference_ID,IsKey,AD_Element_ID,IsEncrypted,IsUpdateable,IsAlwaysUpdateable,Name,ColumnName,Updated,CreatedBy,AD_Client_ID,AD_Org_ID,IsActive,Created,UpdatedBy) VALUES (3004173,298,'LAR',0,'N','N','N','N',40,'N',10,'N',3001036,'N','Y','N','CBU','CBU',TO_DATE('2021-08-04 12:51:12','YYYY-MM-DD HH24:MI:SS'),100,0,0,'Y',TO_DATE('2021-08-04 12:51:12','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 04/08/2021 12:51:13 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=3004173 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 04/08/2021 12:51:13 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Column (AD_Column_ID,AD_Table_ID,EntityType,Version,IsMandatory,IsTranslated,IsIdentifier,Help,IsParent,FieldLength,IsSelectionColumn,AD_Reference_ID,IsKey,AD_Element_ID,IsEncrypted,IsUpdateable,IsAlwaysUpdateable,Description,Name,ColumnName,Updated,CreatedBy,AD_Client_ID,AD_Org_ID,IsActive,Created,UpdatedBy) VALUES (3004174,298,'LAR',0,'N','N','N','The Alias field allows you to define a alternate method for referring to a full account combination.  For example, the Account Receivable Account for Garden World may be aliased as GW_AR.','N',20,'N',10,'N',159,'N','Y','N','Defines an alternate method of indicating an account combination.','Alias','Alias',TO_DATE('2021-08-04 12:51:13','YYYY-MM-DD HH24:MI:SS'),100,0,0,'Y',TO_DATE('2021-08-04 12:51:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 04/08/2021 12:51:13 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=3004174 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 04/08/2021 12:52:23 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Field (IsEncrypted,AD_Field_ID,DisplayLength,IsDisplayed,IsSameLine,IsHeading,AD_Column_ID,IsFieldOnly,IsCentrallyMaintained,AD_Tab_ID,IsReadOnly,Help,EntityType,Description,Name,UpdatedBy,AD_Org_ID,Created,IsActive,AD_Client_ID,Updated,CreatedBy) VALUES ('N',3007324,20,'Y','N','N',3004174,'N','Y',226,'N','The Alias field allows you to define a alternate method for referring to a full account combination.  For example, the Account Receivable Account for Garden World may be aliased as GW_AR.','LAR','Defines an alternate method of indicating an account combination.','Alias',100,0,TO_DATE('2021-08-04 12:52:22','YYYY-MM-DD HH24:MI:SS'),'Y',0,TO_DATE('2021-08-04 12:52:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 04/08/2021 12:52:23 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Help,Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Help,t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=3007324 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 04/08/2021 12:52:23 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Field (IsEncrypted,AD_Field_ID,DisplayLength,IsDisplayed,IsSameLine,IsHeading,AD_Column_ID,IsFieldOnly,IsCentrallyMaintained,AD_Tab_ID,IsReadOnly,EntityType,Name,UpdatedBy,AD_Org_ID,Created,IsActive,AD_Client_ID,Updated,CreatedBy) VALUES ('N',3007325,40,'Y','N','N',3004173,'N','Y',226,'N','LAR','CBU',100,0,TO_DATE('2021-08-04 12:52:23','YYYY-MM-DD HH24:MI:SS'),'Y',0,TO_DATE('2021-08-04 12:52:23','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 04/08/2021 12:52:23 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Help,Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Help,t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=3007325 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 04/08/2021 12:54:46 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field_Trl SET Name='Titular',Description='Titular de la cuenta bancaria.',Help='Titular de la cuenta bancaria.',Updated=TO_DATE('2021-08-04 12:54:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4100 AND AD_Language='es_AR'
+;
+
+-- 04/08/2021 12:54:53 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET IsCentrallyMaintained='N',Updated=TO_DATE('2021-08-04 12:54:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 12:55:34 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field_Trl SET Name='Banco',Description=NULL,Help=NULL,Updated=TO_DATE('2021-08-04 12:55:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2204 AND AD_Language='es_AR'
+;
+
+-- 04/08/2021 12:55:39 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET IsCentrallyMaintained='N',Updated=TO_DATE('2021-08-04 12:55:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:55:49 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_DATE('2021-08-04 12:55:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3007324
+;
+
+-- 04/08/2021 12:57:09 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET DisplayLength=20,Updated=TO_DATE('2021-08-04 12:57:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3007325
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4105
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=40,IsDisplayed='Y' WHERE AD_Field_ID=2203
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=50,IsDisplayed='Y' WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=3007324
+;
+
+-- 04/08/2021 12:57:25 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=100,IsDisplayed='Y' WHERE AD_Field_ID=3007325
+;
+
+-- 04/08/2021 12:58:28 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=3007325
+;
+
+-- 04/08/2021 12:58:28 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=100,IsDisplayed='Y' WHERE AD_Field_ID=3007324
+;
+
+-- 04/08/2021 13:00:02 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=4099
+;
+
+-- 04/08/2021 13:00:02 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 13:00:02 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 13:00:02 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 13:00:02 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=100,IsDisplayed='Y' WHERE AD_Field_ID=3007325
+;
+
+-- 04/08/2021 13:00:02 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=110,IsDisplayed='Y' WHERE AD_Field_ID=3007324
+;
+
+-- 04/08/2021 13:01:03 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field_Trl SET Name='CUIT',Description=NULL,Help=NULL,Updated=TO_DATE('2021-08-04 13:01:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4099 AND AD_Language='es_AR'
+;
+
+-- 04/08/2021 13:01:11 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET IsCentrallyMaintained='N',Updated=TO_DATE('2021-08-04 13:01:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4099
+;
+
+-- 04/08/2021 13:20:09 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET DefaultValue='@SQL=SELECT COALESCE (TaxID) FROM C_BPartner WHERE C_BPartner_ID=@C_BPartner_ID@',Updated=TO_DATE('2021-08-04 13:20:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4099
+;
+
+-- 04/08/2021 13:20:30 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET DefaultValue='@SQL=SELECT COALESCE (Name) FROM C_BPartner WHERE C_BPartner_ID=@C_BPartner_ID@',Updated=TO_DATE('2021-08-04 13:20:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4104
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=50,IsDisplayed='Y' WHERE AD_Field_ID=2204
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=3007325
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=2197
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=3007324
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=4100
+;
+
+-- 04/08/2021 13:44:17 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET SeqNo=100,IsDisplayed='Y' WHERE AD_Field_ID=4099
+;
+
+-- 04/08/2021 13:44:32 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_DATE('2021-08-04 13:44:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3007325
+;
+
+-- 04/08/2021 13:45:47 ART
+-- ISSUE #148: Readecuación de la pestaña Cuenta Bancaria.
+UPDATE AD_Field SET AD_FieldGroup_ID=104,Updated=TO_DATE('2021-08-04 13:45:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2204
+;
+
+-- Registración de script
+SELECT register_migration_script_lar('0189_ISSUE-148.sql', 'LAR', '') FROM dual
+;
