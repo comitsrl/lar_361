84528ff17deb664697d4bc4b60a90417dce20244
Marcos Zúñiga <mzuniga@comit.com.ar>
2024-02-28 20:04:28 -0300

    ISSUE #163: Ajustes en la traducción del Idioma

---
+++ migration/oracle/0224_ISSUE-163.sql
@@ -0,0 +1,11 @@
+UPDATE
+ AD_Message_Trl
+ SET msgtext = 'Existen cantidades disponibles Negativas en este Depósito'
+ WHERE
+ AD_Language='es_AR'
+ AND ad_message_id = 53118
+ ;
+
+-- Registración de script
+SELECT register_migration_script_lar('0224_ISSUE-163.sql', 'LAR', '') FROM dual
+;
